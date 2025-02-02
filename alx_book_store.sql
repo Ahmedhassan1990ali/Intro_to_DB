07e933767e9b248251e44ec2ff6448600d2f6eb9
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2025-02-02 10:40:23 +0200

    week 8 1-3

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -28,13 +28,13 @@ CREATE TABLE Orders(
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE Orders_Details(
+CREATE TABLE Order_Details(
     order_detail_id INT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
 
 ALTER TABLE Books 
